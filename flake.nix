494157971ec6bf3df119f8d58261e6d9e2a1cad8
Hash <hashbangcore@duck.com>
2026-02-22 15:23:17 -0300

    build: bump version to 0.1.2
    - update Cargo.toml and flake.nix with new version

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   in {
     packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
       pname = "netero";
-      version = "0.1.1";
+      version = "0.1.2";
       src = ./.;
       cargoLock = {
         lockFile = ./Cargo.lock;
